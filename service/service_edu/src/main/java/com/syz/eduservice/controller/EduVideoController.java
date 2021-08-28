e07edb1d7203c75c350320472efe0105d1e8d0c3
syz <suyizhen0911@gmail.com>
2021-08-28 19:34:27 +0900

    add video save,update,delete function

--- service/service_edu/src/main/java/com/syz/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/syz/eduservice/controller/EduVideoController.java
@@ -1,9 +1,14 @@
 package com.syz.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.syz.commonutils.R;
+import com.syz.eduservice.entity.EduVideo;
+import com.syz.eduservice.service.EduVideoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -13,8 +18,48 @@ import org.springframework.web.bind.annotation.RestController;
  * @author testjava
  * @since 2021-08-07
  */
+@Api(tags="小节管理")
 @RestController
 @RequestMapping("/eduservice/edu-video")
+@CrossOrigin
 public class EduVideoController {
 
+    @Resource
+    EduVideoService eduVideoService;
+
+    @GetMapping("/getVideoInfoById/{videoId}")
+    public R getVideoInfoById(@PathVariable String videoId){
+        EduVideo video = eduVideoService.getById(videoId);
+        return R.success().data("item",video);
+    }
+
+    @ApiOperation(value = "新增小节")
+    @PostMapping("/addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo){
+        boolean save = eduVideoService.save(eduVideo);
+        if (save){
+            return R.success();
+        }
+        return R.error().code(20010).message("添加小节失败");
+    }
+
+    @ApiOperation(value = "根据ID修改小节")
+    @PostMapping("/updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo){
+        boolean update = eduVideoService.updateById(eduVideo);
+        if (update){
+            return R.success();
+        }
+        return R.error().code(20011).message("修改小节失败");
+    }
+
+    @ApiOperation(value = "根据ID删除小节")
+    @DeleteMapping("/removeVideoById/{videoId}")
+    public R removeVideoById(@PathVariable String videoId){
+        boolean remove = eduVideoService.removeById(videoId);
+        if (remove){
+            return R.success();
+        }
+        return R.error().code(20092).message("删除小节失败");
+    }
 }
